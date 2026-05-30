29db239b28677802e892d7f65eee5a7a6263c01e
OrbitalCheese <sparklingcuzko@gmail.com>
2026-05-30 11:05:59 +0200

    Version bump

--- undiscord-lite.user.js
+++ undiscord-lite.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Undiscord Lite
 // @description Bulk-delete Discord messages. Zero-dependency personal fork of Undiscord with multi-server batching.
-// @version     1.1.0
+// @version     1.1.1
 // @namespace   https://github.com/OrbitalCheese/undiscord-lite
 // @match       https://*.discord.com/app
 // @match       https://*.discord.com/channels/*
